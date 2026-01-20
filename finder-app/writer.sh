5ad7d7389adbb33ed3e26573feb915cbc211db94
dhmo9873 <dhmo9873@colorado.edu>
2026-01-19 22:19:20 -0700

    writer.sh script upodates

---
+++ finder-app/writer.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Count how many arguments passed
+ARG_COUNT=$#
+
+if [ "$ARG_COUNT" -lt 2 ]; then
+    echo "Error: Missing arguments."
+    echo "Usage: $0 <writefile> <writestr>"
+    exit 1
+fi
+
+# Assign the first argument which is a path to a variable
+writefile=$1
+# Assign the second argument which is a string which needs to be written to a filee 
+writestr=$2
+
+dirpath=""
+
+dirpath=$(dirname "$writefile")
+
+# Create the directory if it does not exist
+mkdir -p "$dirpath"
+
+# check to see if directory exists else echo error
+if [ ! -d "$dirpath" ]; then
+    echo "Error: Directory was not created: $dirpath"
+    exit 1
+fi
+
+# Write the string to the file
+echo "$writestr" > "$writefile"
+
+# check if file exists else echo error
+if [ ! -f "$writefile" ]; then
+    echo "Error: File was not created: $writefile"
+    exit 1
+fi
+
+# Exit
+exit 0
+
